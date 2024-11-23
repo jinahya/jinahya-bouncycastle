f2a66b9c2c79f9370ce79aa7f26e2d8c93b44a06
Jin Kwon <onacit@gmail.com>
2024-11-23 21:24:39 +0900

    wip

--- src/main/java/io/github/jinahya/bouncycastle/miscellaneous/__CTR__Constants.java
+++ src/main/java/io/github/jinahya/bouncycastle/miscellaneous/__CTR__Constants.java
@@ -1,10 +1,20 @@
 package io.github.jinahya.bouncycastle.miscellaneous;
 
+/**
+ * Constants for the {@value __CTR__Constants#MODE} mode.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a href="https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Counter_(CTR)">Counter
+ * (CTR)</a>(Wikipedia)
+ */
 @SuppressWarnings({
         "java:S101" // Class names should comply with a naming convention
 })
 public final class __CTR__Constants {
 
+    /**
+     * The mode of {@value}.
+     */
     public static final String MODE = "CTR";
 
     // -----------------------------------------------------------------------------------------------------------------
